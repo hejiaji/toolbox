fc92ef359a90f8a2eadd833848cb6379f1a6b438
Jeremy He <jeremy.he@safetyculture.io>
2021-09-01 18:28:40 +1000

    implement who-goes-first v1

--- packages/who-goes-first/src/components/index.js
+++ packages/who-goes-first/src/components/index.js
@@ -1,38 +1,92 @@
 import React, { useState } from "react";
-import { Typography, Divider, Input, Button, Progress } from "antd";
+import styled from "styled-components";
+import { Typography, Divider, Input, Button, List, Result } from "antd";
 
 const { Title } = Typography;
+
+const Container = styled.div`
+    padding: 0 1rem;
+`;
+
+const BtnAdd = styled(Button)`
+`;
+
+const BtnStart = styled(Button)`
+    margin-left: 0.5rem;
+`;
+
+const Operation = styled.div`
+    margin-top: 1rem;
+`;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
 function WhoGoesFirst() {
     const [players, setPlayers] = useState([]);
     const [inputValue, setInputValue] = useState("");
+    const [playersOrder, setPlayersOrder] = useState([]);
 
     const handPlayersAdd = () => {
         const newPlayers = inputValue.split(",")
-        setPlayers([...players, ...newPlayers]);
+        const allPlayers = [...players, ...newPlayers];
+        setPlayers([...new Set(allPlayers)]);
+    };
+
+    const handleBtnStart = () => {
+        const result = [];
+        let remainingPlayers = [...players];
+        while (remainingPlayers.length > 0) {
+            const picked = getRandomInt(0, remainingPlayers.length);
+            const pickedPlayer = remainingPlayers[picked];
+            result.push(pickedPlayer);
+            remainingPlayers = remainingPlayers.filter(x => x !== pickedPlayer)
+        }
+        setPlayersOrder(result);
+    };
+
+    const renderPlayer = (item) => {
+        return (
+            <List.Item>
+                {item}
+            </List.Item>
+        )
     }
 
-    const renderPlayers = () => {
+    const renderResult = () => {
+        if (playersOrder.length === 0) {
+            return null;
+        }
         return (
-            <div>
-                {players.map((x) =>
-                    (
-                        <div>{x}</div>
-                    ))}
-            </div>
+            <Result
+                status="success"
+                title={`${playersOrder[0]} goes first!`}
+                subTitle={`In order: ${playersOrder.join(",")}`}
+            />
         )
     }
 
     return (
-        <React.Fragment>
+        <Container>
             <Typography>
                 <Title>Who goes first</Title>
                 <Divider/>
             </Typography>
             <Input onChange={(e) => { setInputValue(e.target.value) }} placeholder="Add participants"/>
-            <Button onClick={handPlayersAdd} type="primary">Add</Button>
-            {renderPlayers()}
-        </React.Fragment>
+            <Operation>
+                <BtnAdd onClick={handPlayersAdd} type="primary">Add</BtnAdd>
+                <BtnStart onClick={handleBtnStart}>Start</BtnStart>
+            </Operation>
+            <List
+                size="large"
+                dataSource={players}
+                renderItem={item => renderPlayer(item)}
+            />
+            {renderResult()}
+        </Container>
     )
 }
 
